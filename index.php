7241424ac4c32ff8a6d7aaa8022c1454f5c8c24c
Андрей Гува <guva.andrej.01@gmail.com>
2025-09-18 17:47:16 +0700

    basic routing

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo "Hello world"?>
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+// rudementory routing
+switch ($request) {
+    case '/':
+        require __DIR__ . '/view/index.html';
+        break;
+    case '/admin/import':
+        require __DIR__ . '/view/admin/import.html';
+        break;
+    default:
+        require __DIR__ . '/view/404.html';
+        break;
+}
